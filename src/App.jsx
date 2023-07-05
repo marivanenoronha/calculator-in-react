758df7f045bfbcbb485eefd6ee3e6adc1624d5c6
Marivanenoronha <noronhamarivane4@gmail.com>
2023-07-05 09:28:18 -0300

    add const change input and label

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const [numberOne, seteNumberOne] = useState(0);
+  const [numberTwo, seteNumberTwo] = useState(0);
+  const [result, seteResult] = useState(0);
+  const [opertion, setOperation] = useState("Add")
   
   return (
    <div className='app'>
     <h1>Calculator in react</h1>
     <label>First number</label>
-    <input type='number'/>
+    <input 
+    type='number'
+    value={numberOne}
+    onChange={(e) => seteNumberOne(e.target.value)}/>
     <label>Second number</label>
-    <input type='number'/>
+    <input 
+    type='number'
+    value={numberTwo}
+    onChange={(e) => seteNumberTwo(e.target.value)}
+    />
 
     <select>
       <option>Add</option>
@@ -19,7 +30,7 @@ function App() {
       <option>To divide</option>
     </select>
 
-    <label>Rusult:</label>
+    <label>Rusult:{result}</label>
    </div>
   )
 }
