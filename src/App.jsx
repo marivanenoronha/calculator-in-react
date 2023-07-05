f3d92cf067e5382b34b58d9bad46b429516bec9f
Marivanenoronha <noronhamarivane4@gmail.com>
2023-07-05 09:55:41 -0300

    add const calculate

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,44 @@ function App() {
   const [numberOne, seteNumberOne] = useState(0);
   const [numberTwo, seteNumberTwo] = useState(0);
   const [result, seteResult] = useState(0);
-  const [opertion, setOperation] = useState("Add")
-  
+  const [opertion, setOperation] = useState("Add");
+
+  const calculate = () => {
+    if (opertion == "Add")
+      return parseInt(numberOne) + parseInt(numberTwo);
+    else if (opertion == "Multiply")
+      return parseInt(numberOne) * parseInt(numberTwo);
+    else if (opertion == "Subtract")
+      return parseInt(numberOne) - parseInt(numberTwo);
+    else {
+      return parseInt(numberOne) / parseInt(numberTwo);
+    }
+  }
+
   return (
-   <div className='app'>
-    <h1>Calculator in react</h1>
-    <label>First number</label>
-    <input 
-    type='number'
-    value={numberOne}
-    onChange={(e) => seteNumberOne(e.target.value)}/>
-    <label>Second number</label>
-    <input 
-    type='number'
-    value={numberTwo}
-    onChange={(e) => seteNumberTwo(e.target.value)}
-    />
+    <div className='app'>
+      <h1>Calculator in react</h1>
+      <label>First number</label>
+      <input
+        type='number'
+        value={numberOne}
+        onChange={(e) => seteNumberOne(e.target.value)} />
+      <label>Second number</label>
+      <input
+        type='number'
+        value={numberTwo}
+        onChange={(e) => seteNumberTwo(e.target.value)}
+      />
 
-    <select>
-      <option>Add</option>
-      <option>Multiply</option>
-      <option>Subtract</option>
-      <option>To divide</option>
-    </select>
+      <select onChange={(e) => seteResult(e.target.value)}>
+        <option>Add</option>
+        <option>Multiply</option>
+        <option>Subtract</option>
+        <option>To divide</option>
+      </select>
 
-    <label>Rusult:{result}</label>
-   </div>
+      <label>Rusult:{result}</label>
+    </div>
   )
 }
 
